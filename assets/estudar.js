469b52ff3cf1d115f935663195131be5eb465ae9
Arao <araolubota@gmail.com>
2026-02-10 14:49:39 +0100

    Initial commit: Projecto converson de dolar para real feito em HTML, CSS e JavaScript

---
+++ assets/estudar.js
@@ -0,0 +1,63 @@
+/*Tipos de Variaves
+let idade = 20; //tipo number
+let nome = "Arao Luota"; //tipo String
+let logado = false; // tipo boolean
+
+//Array e objectos
+//let ingredientes = ["Farinha", "Agua", "Sal", "Corante"]
+
+//NOTA ; nomes dos dados de um objeto n'ao se colocar caracteres especias cmo cedilha, acentos apenas nome
+
+let personagens = { 
+    nome: "Lubota",
+    nivel: 10,
+    forca: 800,
+    magia: 200,
+    vidas: 1000,
+    mana: 200
+}
+//funcao
+function somar(a, b){
+    let resultado = a + b;
+    return resultado
+}
+
+let x = somar(15, 5)
+console.log(x)
+
+//arrow function
+let somar2 = (a, b) => {
+    let resultado = a + b
+    return resultado
+}
+
+let y = somar2(2, 8)
+console.log(y)
+
+if (idade >= 18) {
+    console.log("Maior de idade")
+}else{
+    console.log("Menor de idade")
+}
+//Loops em JavaScript
+Let lista = [10, 20, 30 40]
+
+let lista = [10, 20, 30, 40]
+let lista2 = ["Farinha", "agua", "sal", "corante"]
+
+for (let item of lista2){
+    console.log(item)
+}
+
+
+//let usdInput = document.querySelector("#usd")
+l//et brlInput = document.querySelector("#brl")
+
+//usdInput.addEventListener("keyup", () => {
+   // console.log("Apertou no campo USD")
+//})
+
+//brlInput.addEventListener("keyup", () =>{
+    //console.log("Apertou no campo BRL")
+//})
+*/
